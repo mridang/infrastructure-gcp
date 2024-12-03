445f7b81a492820d76d1f30ef5907bf6adcf699a
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2024-12-03 08:09:12 +0700

    Enabled log analytics for the default project

--- index.ts
+++ index.ts
@@ -7,6 +7,26 @@ import {argoTunnel} from './src/instances'
 import './src/billing'
 import {serviceUri} from './src/services/mycontainer'
 
+/**
+ * Logging buckets are automatically created for a given folder, project,
+ * organization, billingAccount and cannot be deleted. Creating a resource of
+ * this type will acquire and update the resource that already exists at the
+ * desired location. These buckets cannot be removed so deleting this resource
+ * will remove the bucket config from your state but will leave the logging
+ * bucket unchanged.
+ * The buckets that are currently automatically created are “_Default” and
+ * “_Required”.
+ *
+ * By default, the logging retention is 30 days and log analytics are disabled
+ */
+new gcp.logging.ProjectBucketConfig("basic", {
+	project: gcp.config.project || '',
+	location: "global",
+	retentionDays: 14,
+	enableAnalytics: true,
+	bucketId: "_Default",
+});
+
 // 1. Create a GCP Storage Bucket to store the function source code
 const functionBucket = new gcp.storage.Bucket("function-code-bucket", {
 	location: "US",
@@ -55,5 +75,5 @@ const functionInvokerBinding = new gcp.cloudfunctions.FunctionIamMember("helloWo
 // Export the function URL for testing
 export const url = serviceUri
 export const functionUrl = cloudFunction.httpsTriggerUrl;
-export const  vpcid = defaultVPC
+export const vpcid = defaultVPC
 export const argoTunne = argoTunnel
