b621d8bbde53cc0ef72d71cdaf7ad24ec248a8cd
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2024-12-03 09:14:14 +0700

    Added a firewall rule to allow ipv6 traffic between the nodes

--- src/vpc.ts
+++ src/vpc.ts
@@ -13,11 +13,33 @@ const vpc = new gcp.compute.Network("default-vpc", {
  * to be a "regular subnet". https://cloud.google.com/vpc/docs/subnets#purpose
  */
 const subnet = new gcp.compute.Subnetwork("default-subnet", {
-	ipCidrRange: "10.0.0.0/24", // CIDR range
-	region: "us-central1", // Choose your region
+	ipCidrRange: "10.0.0.0/24",
+	region: "us-central1",
 	network: vpc.id,
 	ipv6AccessType: "INTERNAL", // Internal IPv6 traffic only
 	stackType: "IPV4_IPV6", // Enables both IPv4 and IPv6
+});
+
+/**
+ * Firewall rule to allow internal ipv6 traffic between services and nodes inside
+ * the VPC.
+ */
+new gcp.compute.Firewall("allow-internal-ipv6", {
+	network: vpc.id,
+	allows: [
+		{
+			protocol: "tcp",
+			ports: ["0-65535"],
+		},
+		{
+			protocol: "udp",
+			ports: ["0-65535"],
+		},
+		{
+			protocol: "58", // An alias for ICMPv6 as since GCP doesn't understand
+		},
+	],
+	sourceRanges: ["fd00::/8"],
 });
 
 /**
