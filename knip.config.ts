b8f3d0532224603eef1ffe3b07a74b83f4d46e36
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-06-14 11:57:02 +0700

    fix(knip): trace pulumi project entries
    - add entry globs for projects/*/src and Pulumi.yaml
    - ignore pulumi binary (invoked at deploy time)

--- knip.config.ts
+++ knip.config.ts
@@ -1,4 +1,11 @@
 export default {
+  entry: [
+    'projects/*/src/index.ts',
+    'projects/*/src/**/*.ts',
+    'Pulumi.yaml',
+    'projects/*/Pulumi.yaml',
+  ],
   ignore: ['knip.config.ts'],
   ignoreDependencies: [/^@semantic-release\//],
+  ignoreBinaries: ['pulumi'],
 };
